401d20efc74712b3b093ae22c71cb8d846a6fcde
tfshorifulislam <tfshorifulislam@gmail.com>
2026-05-22 22:39:49 +0600

    navbar done

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@ import Navbar from "../components/Navbar"
 import Providers from "./providers"
 
 import { IBM_Plex_Serif } from "next/font/google"
-import SideBar from "../components/SideBar"
 import MobileButtonMenuBar from "@/components/MobileButtomMenuBar"
 import { Toaster } from "react-hot-toast"
 
@@ -21,8 +20,6 @@ export default function RootLayout({ children }) {
         className={`${ibmPlexSerif.className}`}>
         <Providers>
           <Navbar />
-          {/* <SideBar /> */}
-
           <main className="min-h-screen">
             {children}
           </main>
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,48 @@
-import React from 'react';
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div>
-      
+    <div className="w-11/12 mx-auto mt-5 columns-1 sm:columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-4 space-y-4">
+
+      {posts.map((post) => (
+        <div
+          key={post._id}
+          className="break-inside-avoid rounded-xl bg-white dark:bg-neutral-950 border border-neutral-200 dark:border-neutral-800 overflow-hidden mb-4"
+        >
+          {post.text && (
+            <p className="p-3 text-sm text-neutral-800 dark:text-neutral-200">
+              {post.text}
+            </p>
+          )}
+
+          {post.imageUrl && (
+  <div className="relative w-full aspect-auto">
+
+    <Image
+      src={post.imageUrl}
+      alt="post"
+      width={800}
+      height={800}
+      className="w-full h-auto object-cover"
+      unoptimized
+    />
+
+  </div>
+)}
+        </div>
+      ))}
+
     </div>
   );
 };
